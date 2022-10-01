af40e77454d09696343bac37778b00b918b55672
Glitch (glitch-hello-website) <none>
2022-10-01 00:18:01 +0000

    📉🚌 Checkpoint ./TODO.md:23396706/5818 ./TODO.txt:23396706/12 ./script.js:23396706/1213 ./index.html:23396706/212

---
+++ src/js/script.js
@@ -0,0 +1,9 @@
+
+import { scythe } from "/js/libraries/scythe/scythe.js";
+var $, append, prepend, ele, txt, atr;
+$ = scythe.pkg.query;
+append = scythe.pkg.element.append;
+prepend = scythe.pkg.element.prepend;
+ele = scythe.pkg.element.create;
+txt = scythe.pkg.element.text;
+atr = scythe.pkg.element.attribute;
