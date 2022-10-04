bd6295fcfb412de5cf6713abc8a3c11414bcc738
Glitch (glitch-hello-website) <none>
2022-10-04 23:10:00 +0000

    🍬💁 Checkpoint ./app/index.html:23396706/316 ./src/def/js/init.js:23396706/740

--- src/def/js/init.js
+++ src/def/js/init.js
@@ -15,24 +15,10 @@ This web app is a product of
 All rights reserved.    
 */
 
-import { scythe } from "https://epicode.glitch.me//js/libraries/scythe/scythe.js";
-
-var $, ss, ls, move, loc, $class, append, prepend, element, txt, atr, getRandom;
-$ = scythe.pkg.query;
-ss = scythe.pkg.sessionStorage;
-ls = scythe.pkg.localStorage;
-move = scythe.pkg.element.move;
-loc = scythe.pkg.element.location;
-$class = scythe.pkg.classList;
-append = scythe.pkg.element.append;
-prepend = scythe.pkg.element.prepend;
-element = scythe.pkg.element.create;
-txt = scythe.pkg.element.text;
-atr = scythe.pkg.element.attribute;
-getRandom = scythe.pkg.random;
-
 window.onload = function() {
-  $("#loadingScreen").style.display = "none";
+  document.querySelector("#loadingScreen").style.display = "none";
 }
 
-// append according to version here
+$.get("/src/data/updates.json", function(data){
+  alert("Data: " + data);
+});
