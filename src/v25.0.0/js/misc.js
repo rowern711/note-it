b1f7a6827d45919cb9e6bcf7fefe322bbf8d594f
Glitch (glitch-hello-website) <none>
2022-10-04 22:57:59 +0000

    🏟🙅 Checkpoint ./src/def/js/misc.js:23396706/860 ./src/v25.0.0/js/misc.js:23396706/1823 ./src/data/updates.json:23396706/585

---
+++ src/v25.0.0/js/misc.js
@@ -0,0 +1,36 @@
+/* 
+ALL CODE IS PURLEY ORIGINAL.
+
+YOU MAY NOT COPY, MODIFY, MERGE, PUBLISH, DISTIBUTE, SUBLICENCE, AND/OR SELL THE CODE. 
+
+This web app is a product of
+
+███████╗██████╗ ██╗ ██████╗ ██████╗ ██████╗ ███████╗
+██╔════╝██╔══██╗██║██╔════╝██╔═══██╗██╔══██╗██╔════╝
+█████╗  ██████╔╝██║██║     ██║   ██║██║  ██║█████╗  
+██╔══╝  ██╔═══╝ ██║██║     ██║   ██║██║  ██║██╔══╝  
+███████╗██║     ██║╚██████╗╚██████╔╝██████╔╝███████╗
+╚══════╝╚═╝     ╚═╝ ╚═════╝ ╚═════╝ ╚═════╝ ╚══════╝
+
+All rights reserved.    
+*/
+
+import { scythe } from "https://epicode.glitch.me//js/libraries/scythe/scythe.js";
+
+var $, ss, ls, move, loc, $class, append, prepend, element, txt, atr, getRandom;
+$ = scythe.pkg.query;
+$class = scythe.pkg.classList;
+
+
+$("#btn-continue-browser").onclick = function () {
+  $class.add($("#warn-not_compatible_browser"), "page-out");
+  setTimeout(function () {
+    $("#warn-not_compatible_browser").style.display = "none";
+  }, 1000);
+};
+$("#btn-continue-iframe").onclick = function () {
+  $class.add($("#warn-not_compatible_iframe"), "page-out");
+  setTimeout(function () {
+    $("#warn-not_compatible_iframe").style.display = "none";
+  }, 1000);
+};
