0db22fe10344621342a9f292ba770cbde048b401
Glitch (glitch-hello-website) <none>
2022-10-04 23:45:11 +0000

    👨👧 Checkpoint ./src/def/js/version.js:23396706/7975 ./app/index.html:23396706/9

--- src/def/js/version.js
+++ src/def/js/version.js
@@ -16,20 +16,42 @@ All rights reserved.
 */
 
 var data = localStorage.getItem("data");
+var client = {
+  version: data
+  // function () {
+  //   JSON.parse(localStorage.getItem("data"))["client"]["version"];
+  // },
+};
+
+console.log(client.version);
 
 if (data == null) {
-  $.get("/src/data/updates.json", function(data){
-    var data_base = {
-      app: {
-        version: data["latest_version"]
-      }
-    };
-    localStorage.setItem("data", JSON.stringify(data_base));
-    
-  });
+  func.autoupdate();
 } else {
-  
+  if (client.version == null) {
+    func.autoupdate();
+  }
 }
+
+var func = {
+  autoupdate: function () {
+    $.get("/src/data/updates.json", function (data) {
+      var latest_version = data["latest_version"];
+      var data_base = {
+        app: {
+          version: latest_version,
+        },
+      };
+      localStorage.setItem("data", JSON.stringify(data_base));
+      alert(
+        "The latest version of Note It (v" +
+          latest_version +
+          ") has been automatically installed."
+      );
+    });
+  },
+};
 
 // var version = JSON.parse(ls.get("data"))["app"]["version"];
 
+// JSON.parse(localStorage.getItem("data"))["client"]["version"]
